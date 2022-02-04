25af6139f3b90512df77824dbf797f0878d3f8f9
Aldrin Cheung <aldrin.cheung@gmail.com>
2022-02-04 15:29:01 -0800

    a initialized codebase commit with a failed test

--- Division.java
+++ Division.java
@@ -1,4 +1,14 @@
 public class Division{
+    /**
+     * to calculate x/y
+     * @param x to be divide
+     * @param y divide by this number
+     * @return x/y
+     */
+    public double divide(double x, double y){
+        return x/y + 1; //to make it false at first
+    }
+
     public static void main(String[] args){
         System.out.println("hellow world");
     }
---
+++ DivisionTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DivisionTest{
+    @Test
+    public void testDivide(){
+        Division division = new Division();
+        double x = 1;
+        double y = 2;
+        assertEquals(x/y, division.divide(x, y));
+    }
+}
